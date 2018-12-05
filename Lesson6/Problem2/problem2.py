d4924a8b058fb96662873731bf3d60a394095710
ariiellaa <44405336+ariiellaa@users.noreply.github.com>
2018-12-05 12:44:43 -0500

    Add files via upload

---
+++ Lesson6/Problem2/problem2.py
@@ -0,0 +1,9 @@
+name = input(' What is your name? ') 
+print('Hello ' + name + '!')
+
+
+print('Nice to meet you ' + name +', my name is Chatbot.')
+age = input('How old are you? ')
+print('wow, ' + age + ' years old! I was born today.')
+feeling = input('How are you feeling today? ')
+print('Glad to hear you are feeling ' + feeling.lower() + '. That is wonderful to hear!')
---
+++ Lesson6/Problem3/Problem3.py
@@ -0,0 +1,7 @@
+
+name = input(' What is your name? ') 
+print('UGH, I HATE YOU ' + name + ".")
+color = input('What is your favorite color? ')
+print('UGH, I HATE ' + color + ', THERE ARE SO MANY BETTER COLORS. ')
+subject = input('What is your favorite subject in school? ')
+print('UGH, I HATE ' + subject + '. I HATE EVERYTHING!')
